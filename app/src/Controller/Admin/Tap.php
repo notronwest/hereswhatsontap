3e2527fd1051939b2edd97d183c36ad31b3b64f3
Ron West <ron@inquisibee.com>
2018-03-29 23:27:19 -0400

    * updated twig package so that templates in /templates directory can be referenced with an alias * created taps home page * created taps form * updated user repository to get customer based on user * added base controller with getCurrentCustomer() function * moved tap twig templates under tap folder * updated user to know about customer (may back that out in place of repository/base controller approach)

--- app/src/Controller/Admin/Tap.php
+++ app/src/Controller/Admin/Tap.php
@@ -6,15 +6,38 @@
  * Time: 8:38 PM
  */
 
-namespace App\Controller\Admin;
+namespace App\Controller\Admin\Tap;
 
+use App\Controller\BaseController;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Tap as TapEntity;
 
 
-class Tap
+class Tap extends BaseController
 {
-    public function indexAction(EntityManagerInterface $entityManager){
 
+    /**
+     * @Route("/admin/taps", name="taps")
+     */
+    public function indexAction()
+    {
+
+        $Taps = $this->getDoctrine()
+            ->getRepository(TapEntity::class)
+            ->getTaps($this->getCurrentCustomer());
+
+        return $this->render("@App/admin/tap/taps.html.twig",
+            [ 'taps' => $Taps ]);
+
+    }
+
+    /**
+     * @Route("/admin/taps/edit/{tap_id)", name="editTap")
+     */
+    public function editAction()
+    {
+        return $this->render("@App/admin/tap/edittap.html.twig");
 
     }
 }
